fd2395b66143945ed50c2ff42d3a156368ff1ef2
HawkeyeUW <virtuej@outlook.com>
2021-02-21 21:56:44 -0500

    calc assignment

--- calculator.py
+++ calculator.py
@@ -40,45 +40,118 @@ To submit your homework:
 
 
 """
+def main():
+    body = ['<h1>WSGI Calculator</h1>', '<ul>']
+    body.append('<h3>This calculator will do simple  +,  -,  * and  / with two numbers.</h3>')
+    body.append('<ul>')
+    body.append('<html>Instructions......</html>')
+    body.append('<li>First parameter will be add, subtract, multiply or divide</li>')
+    body.append('<li>Second parameter of path will be the first number</li>')
+    body.append('<li>Third parameter of the path will be the second number</li>')
+    body.append('<li>Example: /add/5/2 will yield 7</li>')
 
+    return '\n'.join(body)
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+    digit1 = args[0]
+    digit1 = int(digit1)
 
-    return sum
+    digit2 = args[1]
+    digit2 = int(digit2)
 
-# TODO: Add functions for handling more arithmetic operations.
+    sum = digit1 + digit2
+
+    page = ['<h1>Addition Calculator Results</h1>']
+    page.append(f'<html> {digit1} + {digit2} = {sum}<html>')
+    return '\n'.join(page)
+
+def subtract(*args):
+    digit1 = args[0]
+    digit1 = int(digit1)
+
+    digit2 = args[1]
+    digit2 = int(digit2)
+
+    subtract = digit1 - digit2
+
+    page = ['<h1>Subtraction Calculator Results</h1>']
+    page.append(f'<html> {digit1} - {digit2} = {subtract}<html>')
+    return '\n'.join(page)
+
+def multiply(*args):
+    digit1 = args[0]
+    digit1 = int(digit1)
+
+    digit2 = args[1]
+    digit2 = int(digit2)
+
+    multiply = digit1 * digit2
+
+    page = ['<h1>Multiplication Calculator Results</h1>']
+    page.append(f'<html> {digit1} * {digit2} = {multiply}<html>')
+    return '\n'.join(page)
+
+def divide(*args):
+    digit1 = args[0]
+    digit1 = int(digit1)
+
+    digit2 = args[1]
+    digit2 = int(digit2)
+
+    divide = digit1 / digit2
+
+    page = ['<h1>Division Calculator Results</h1>']
+    page.append(f'<html> {digit1} / {digit2} = {divide}<html>')
+    return '\n'.join(page)
 
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
+    funcs = {
+        '': main,
+        'add': add,
+        'subtract': subtract,
+        'multiply': multiply,
+        'divide': divide
+    }
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
 
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1> Internal Server Error</h1>"
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
